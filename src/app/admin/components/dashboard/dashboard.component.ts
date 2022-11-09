096e811b7814818163bb55eab85dd5cd0478ee67
Eray <eraybrbr@gmail.com>
2022-11-09 03:14:34 +0300

    Add SignalRHub for Creating Orders.

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HubUrls } from 'src/app/constants/hub-urls';
+import { ReceiveFunctions } from 'src/app/constants/receive-functions';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { SignalRService } from 'src/app/services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,12 +13,19 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner: NgxSpinnerService) {
+  constructor(private alertify: AlertifyService, spinner: NgxSpinnerService, private signalRService: SignalRService) {
     super(spinner)
+    signalRService.start(HubUrls.OrderHub)
+    signalRService.start(HubUrls.ProductHub)
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.SquareLoader);
+    this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+      this.alertify.notification(message, MessageType.Notify, Position.TopRight)
+    });
+    this.signalRService.on(ReceiveFunctions.OrderCreatedMessageReceiveFunction, message => {
+      this.alertify.notification(message, MessageType.Notify, Position.TopCenter)
+    });
   }
 
 }
--- src/app/ui/components/baskets/baskets.component.ts
+++ src/app/ui/components/baskets/baskets.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { List_Basket_Item } from 'src/app/contracts/basket/list_basket_item';
 import { Update_Basket_Item } from 'src/app/contracts/basket/update_basket_item';
+import { Create_Order } from 'src/app/contracts/order/create_order';
 import { BasketService } from 'src/app/services/common/models/basket.service';
+import { OrderService } from 'src/app/services/common/models/order.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/CustomToastr.service';
 
 declare var $: any;
 
@@ -14,7 +18,11 @@ declare var $: any;
 })
 export class BasketsComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner: NgxSpinnerService, private basketService: BasketService) {
+  constructor(spinner: NgxSpinnerService,
+    private basketService: BasketService,
+    private orderService: OrderService,
+    private toastrService: CustomToastrService,
+    private router:Router) {
     super(spinner)
   }
 
@@ -43,5 +51,16 @@ export class BasketsComponent extends BaseComponent implements OnInit {
 
     var a = $("." + basketItemId)
     $("." + basketItemId).fadeOut(500, () => this.hideSpinner(SpinnerType.BallPulse));
+  }
+
+  async confirmOrder() {
+    this.showSpinner(SpinnerType.SquareLoader);
+    const order: Create_Order = new Create_Order();
+    order.address = "İzmir/Buca";
+    order.description = "A4 Tech Webcam";
+    await this.orderService.create(order)
+    this.hideSpinner(SpinnerType.SquareLoader);
+    this.toastrService.notification("Order completed.", "Success", ToastrMessageType.Info, ToastrPosition.TopCenter);
+    this.router.navigate(["/"])
   }
 }
